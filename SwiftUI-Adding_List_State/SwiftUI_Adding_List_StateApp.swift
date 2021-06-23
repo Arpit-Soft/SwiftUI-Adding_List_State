8334102d67bb23e7cd86124dabb58a37c0e6fe19
Arpit Dixit <arpitd@dewsolutions.in>
2021-06-23 17:06:13 +0530

    Initial Commit

---
+++ SwiftUI-Adding_List_State/SwiftUI_Adding_List_StateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Adding_List_StateApp.swift
+//  SwiftUI-Adding_List_State
+//
+//  Created by Arpit Dixit on 23/06/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Adding_List_StateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
